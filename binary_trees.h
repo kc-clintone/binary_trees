c50b8b0f3a4e6300f3c98cf83c79c20ca034ade4
kc-clintone <kayseeclintone@gmail.com>
2024-03-27 20:50:41 +0000

    binary_trees >>> task 16

--- binary_trees.h
+++ binary_trees.h
@@ -26,6 +26,8 @@ struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
 
+/**---others---*/
+int pwr_r(int n, int p);
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
