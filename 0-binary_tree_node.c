a98b1c2ffe16af33f8bccdb6d036b7080ff97506
kc-clintone <kayseeclintone@gmail.com>
2024-03-27 17:08:49 +0000

    binary_trees >>> task 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - This unction that inserts a node as
+ * the left-child of another node
+ *
+ * @parent: - Pointer to the parent node of the node to create
+ * @value: - Value to put in the new node
+ * Return: Pointer to new node, NULL otherwise
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+binary_tree_t *new_node;
+
+new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+if (new_node == NULL)
+return (NULL);
+new_node->parent = parent;
+new_node->n = value;
+new_node->left = NULL;
+new_node->right = NULL;
+return (new_node);
+}
