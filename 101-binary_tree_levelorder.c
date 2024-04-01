b1a6d76493b403f362fa5ad96fecdfbc9edbf2a8
kc-clintone <kayseeclintone@gmail.com>
2024-04-01 14:43:54 +0000

    corrections

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -27,10 +27,8 @@ void fn_1(const binary_tree_t *tree, void (*func)(int), size_t level)
 if (level == 1)
 {
 func(tree->n);
-}
 else
-{
-fn_1(tree->left, func, level - 1)
+fn_1(tree->left, func, level - 1);
 fn_1(tree->right, func, level - 1);
 }
 }
