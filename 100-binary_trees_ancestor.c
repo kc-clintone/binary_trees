7ef4ca5a51a9fdf6e35c302170a6f8d10760d617
kc-clintone <kayseeclintone@gmail.com>
2024-04-01 14:33:05 +0000

    corrections

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -37,3 +37,23 @@ second = second->parent;
 }
 return (NULL);
 }
+
+/**
+ * binary_tree_depth - Thid function measures the depth of a node in
+ * a binary tree
+ * @tree: Target node
+ * Return: Depth | 0 if tree is NULL
+*/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+size_t x = 0;
+
+if (!tree)
+return (0);
+while (tree->parent)
+{
+x++;
+tree = tree->parent;
+}
+return (x);
+}
