35cee970af010cf0f4df696e41f1e644b87c6e0b
kc-clintone <kayseeclintone@gmail.com>
2024-04-11 14:48:32 +0000

    corrections

---
+++ 132-array_to_heap.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h
+/**
+ * array_to_heap - This just builds a Max Binary Heap
+ * tree from an array
+ * @array: Pointrr to target
+ * @size: Number of elements in array
+ * Return: Pointer or NULL on failure
+*/
+heap_t *array_to_heap(int *array, size_t size)
+{
+unsigned int x;
+heap_t *rt = NULL;
+
+for (x = 0; x < size; x++)
+heap_insert(&rt, array[x]);
+return (rt);
+}
